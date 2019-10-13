fa345d06d26f9071a6d875965487f880645f9b87
Bojan Komljenović <bojan.komljenovic@infinum.hr>
2019-10-13 18:00:01 +0200

    Stuck

--- plugin/src/main/groovy/co/infinum/collar/plugin/CollarPlugin.groovy
+++ plugin/src/main/groovy/co/infinum/collar/plugin/CollarPlugin.groovy
@@ -8,20 +8,17 @@ class CollarPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         project.dependencies {
+            implementation "org.aspectj:aspectjrt:1.9.4"
             implementation 'co.infinum.collar:collar-core:1.0.0'
         }
-        project.getPluginManager().apply("com.archinamon.aspectj")
-        println("PLUGIN APPLIED - BOJAN IS AWESOME!!!3")
-        project.extensions.getByType(AspectJExtension.class)['includeAspectsFromJar'] = 'core'
-        println("PLUGIN SETUP - BOJAN IS AWESOME!!!4")
 
         project.android.applicationVariants.all { variant ->
             def variantName = variant.name[0].toUpperCase() + variant.name[1..-1].toLowerCase()
             project.tasks.findByName('compile' + variantName + 'Sources')?.doLast {
-                project.tasks.getByName('transformClassesWithAspectjFor' + variantName)
+
             }
             project.tasks.findByName('compile' + variantName + 'UnitTestSources')?.doLast {
-                project.tasks.getByName('transformClassesWithAspectjFor' + variantName + 'AndroidTest')
+
             }
         }
     }
