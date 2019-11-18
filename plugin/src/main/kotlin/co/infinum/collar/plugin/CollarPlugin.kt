703139f1e635f678857ee52a7183d7fe9b727a39
Bojan Komljenović <bojan.komljenovic@infinum.hr>
2019-11-18 22:15:16 +0100

    Publish all packages.

--- plugin/src/main/kotlin/co/infinum/collar/plugin/CollarPlugin.kt
+++ plugin/src/main/kotlin/co/infinum/collar/plugin/CollarPlugin.kt
@@ -14,16 +14,15 @@ class CollarPlugin : Plugin<Project> {
     private fun addDependencies(project: Project, settings: CollarExtension) {
         with(project) {
             with(repositories) {
+                google()
                 jcenter()
                 mavenCentral()
                 maven {}.url = URI("https://dl.bintray.com/knobtviker/maven")
                 maven {}.url = URI("http://dl.bintray.com/infinum/android")
             }
             with(dependencies) {
+                add("implementation", "androidx.core:core-ktx:1.1.0")
                 add("implementation", "co.infinum.collar:collar-core:" + settings.version)
-                add("implementation", "co.infinum.collar:collar-annotations:" + settings.version)
-                // TODO: What if it is not a Kotlin project??? kapt won't work then.
-                add("kapt", "co.infinum.collar:collar-processor:" + settings.version)
             }
         }
     }
