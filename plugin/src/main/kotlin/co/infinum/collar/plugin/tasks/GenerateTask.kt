4c692cd5db9f941253dc5cf4bb9d3eea2856bf64
Mladen Rakonjac <mladenbp@gmail.com>
2020-03-02 16:39:20 +0100

    Added local build and integrated collarGenerate task

--- plugin/src/main/kotlin/co/infinum/collar/plugin/tasks/GenerateTask.kt
+++ plugin/src/main/kotlin/co/infinum/collar/plugin/tasks/GenerateTask.kt
@@ -2,6 +2,7 @@ package co.infinum.collar.plugin.tasks
 
 import co.infinum.collar.plugin.CollarExtension
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.TaskExecutionException
 
 open class GenerateTask : BaseTask() {
 
@@ -13,6 +14,18 @@ open class GenerateTask : BaseTask() {
     fun generateStubFiles() {
         val extension: CollarExtension =
             project.extensions.findByName("collar") as CollarExtension
+        if (extension.filePath.isBlank()) {
+            throw  TaskExecutionException(
+                this,
+                Exception("Task collarGenerate failed; filePath should be specified")
+            )
+        }
+        if (extension.outputPath.isBlank()) {
+            throw  TaskExecutionException(
+                this,
+                Exception("Task collarGenerate failed; outputPath should be specified")
+            )
+        }
         taskUtils.logger.logWarning("Extension file path:")
         taskUtils.logger.logWarning(extension.filePath)
         taskUtils.logger.logWarning("Files will be generated on path:")
