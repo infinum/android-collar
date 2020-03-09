138e8d28946283aa6e5969a7c454780d55e6f8ae
Mladen Rakonjac <mladenbp@gmail.com>
2020-03-09 16:45:07 +0100

    Deletes progress

--- generator/src/main/java/co/infinum/generator/utils/SynchronousExecutor.kt
+++ generator/src/main/java/co/infinum/generator/utils/SynchronousExecutor.kt
@@ -7,11 +7,9 @@ import java.util.concurrent.TimeUnit
 class SynchronousExecutor {
 
     private val executor: ExecutorService = Executors.newSingleThreadExecutor()
-    private val progress = Progress()
 
     fun start(task: Runnable) {
         executor.execute(task)
-        progress.start()
     }
 
     fun await() {
@@ -21,27 +19,7 @@ class SynchronousExecutor {
     }
 
     private fun stop() {
-        hideProgress()
         println()
         System.out.flush()
-    }
-
-    private fun hideProgress() {
-        progress.showProgress = false
-    }
-
-    private class Progress : Thread() {
-
-        private val PROGRESS_PERIOD = 200L // ms
-
-        var showProgress = true
-
-        override fun run() {
-            while(showProgress) {
-                print(".")
-                System.out.flush()
-                Thread.sleep(PROGRESS_PERIOD)
-            }
-        }
     }
 }
