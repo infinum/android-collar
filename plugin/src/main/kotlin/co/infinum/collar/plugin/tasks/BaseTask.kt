576a1c0574fb3cecfea6d564671deecd2518ea7f
Mladen Rakonjac <mladenbp@gmail.com>
2020-03-09 16:50:04 +0100

    Deletes jansi

--- plugin/src/main/kotlin/co/infinum/collar/plugin/tasks/BaseTask.kt
+++ plugin/src/main/kotlin/co/infinum/collar/plugin/tasks/BaseTask.kt
@@ -2,7 +2,6 @@ package co.infinum.collar.plugin.tasks
 
 import co.infinum.generator.GeneratorLib
 import co.infinum.generator.logging.Logger
-import org.fusesource.jansi.Ansi
 import org.gradle.api.DefaultTask
 
 open class BaseTask: DefaultTask() {
@@ -13,11 +12,11 @@ open class BaseTask: DefaultTask() {
     init {
         generatorLib = GeneratorLib(logger = object : Logger {
             override fun logError(message: String) {
-                logger.error(Ansi.ansi().fg(Ansi.Color.RED).a(message).reset().toString())
+                logger.error(message)
             }
 
             override fun logSuccess(message: String) {
-                logger.quiet(Ansi.ansi().fg(Ansi.Color.GREEN).a(message).reset().toString())
+                logger.quiet(message)
             }
 
             override fun logDebug(message: String) {
@@ -26,7 +25,7 @@ open class BaseTask: DefaultTask() {
 
             override fun logWarning(message: String) {
                 // warn will not be shown in quiet mode
-                logger.error(Ansi.ansi().fg(Ansi.Color.YELLOW).a(message).reset().toString())
+                logger.error(message)
             }
 
         })
