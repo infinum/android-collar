b8d816d01e1b10d98731907b832f8e2aaa1aa3ec
Bojan Komljenović <bojan.komljenovic@infinum.hr>
2020-05-11 00:28:45 +0200

    Initial setup for ContentProvider

--- ui/src/main/kotlin/co/infinum/collar/ui/CollarContentProvider.kt
+++ ui/src/main/kotlin/co/infinum/collar/ui/CollarContentProvider.kt
@@ -2,11 +2,30 @@ package co.infinum.collar.ui
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.Context
+import android.content.pm.ProviderInfo
 import android.database.Cursor
 import android.net.Uri
 import co.infinum.collar.ui.presentation.Presentation
 
 internal class CollarContentProvider : ContentProvider() {
+
+    companion object {
+        private const val DEFAULT_PACKAGE = "co.infinum.collar.ui"
+    }
+
+    override fun attachInfo(context: Context?, info: ProviderInfo?) {
+        info?.let {
+            if ("$DEFAULT_PACKAGE.${CollarContentProvider::class.java.simpleName}" == it.authority) {
+                throw IllegalStateException(
+                    "Incorrect provider authority. " +
+                        "Most likely due to missing applicationId variable in module build.gradle."
+                )
+            }
+        } ?: throw IllegalStateException("This component cannot work with null ProviderInfo.")
+
+        super.attachInfo(context, info)
+    }
 
     override fun onCreate(): Boolean =
         context?.let {
