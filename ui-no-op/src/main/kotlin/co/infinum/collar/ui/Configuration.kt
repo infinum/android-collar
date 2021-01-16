c132130fdbeb38e4b79dd594c3fada7314a37a15
Bojan <bojan.komljenovic@infinum.hr>
2021-01-16 22:33:22 +0100

    Fix a Configuration class in no op package.

--- ui-no-op/src/main/kotlin/co/infinum/collar/ui/Configuration.kt
+++ ui-no-op/src/main/kotlin/co/infinum/collar/ui/Configuration.kt
@@ -2,6 +2,8 @@ package co.infinum.collar.ui
 
 public data class Configuration(
 
+    val analyticsCollectionEnabled: Boolean = false,
+
     val showSystemNotifications: Boolean = false,
 
     val showInAppNotifications: Boolean = false,
